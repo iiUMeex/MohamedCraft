2eebd86c602fab4df5eacc018a8ca0234bbed95f
iiUMeex <41113180+iiUMeex@users.noreply.github.com>
2018-08-19 06:59:31 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,8 +61,7 @@ message.channel.send('**لديك 15 ثانية لكتابة الكلمة**').the
 }
 });
 if (message.content.startsWith(prefix + 'نقاطي')) {
-	if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
-	let userData = points[message.author.id];
+=	let userData = points[message.author.id];
 	let embed = new Discord.RichEmbed()
     .setAuthor(`${message.author.tag}`, message.author.avatarURL)
 	.setColor('#000000')
