0570745255efad563bba0bd5117578abe7cde2b4
ZALABIA <41113180+iiUMeex@users.noreply.github.com>
2019-02-22 13:42:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,6 @@ client.on('message', msg => {
 
 const pretty = require('pretty-ms'); // npm i pretty-ms
 const credits = require('./Credits.json');
-const creditsPath = './Credits.json';
 client.on('message',async message => {
     if(message.author.bot || message.channel.type === 'dm') return;
     let args = message.content.split(' ');
