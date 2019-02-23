50f8b1771e7954a07453fba6af2a5be70a53e65e
ZALABIA <41113180+iiUMeex@users.noreply.github.com>
2019-02-23 17:43:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -108,9 +108,9 @@ mentionned.send(`:credit_card: | Transfer Receipt \`\`\`\`You have received ${ar
         })
         })
 }
-	if (message.content.toUpperCase() === `${prefix}payyou`) {
+	if (message.content.toUpperCase() === `${prefix}payme`) {
 
-            money.updateBal(message.author.id, 1000000) //.then((i) => { // money.updateBal grabs the (userID, value) value being how much you want to add, and puts it into 'i'.
+            profile.updateBal(message.author.id, 1000000) //.then((i) => { // money.updateBal grabs the (userID, value) value being how much you want to add, and puts it into 'i'.
                  message.channel.send(`**You got $1000000!**\n**New Balance:** ${i.money}`);
             
  
@@ -118,7 +118,7 @@ mentionned.send(`:credit_card: | Transfer Receipt \`\`\`\`You have received ${ar
  
         if (message.content.toUpperCase() === `${prefix}payfine`) {
  
-            money.updateBal(message.author.id, -500).then((i) => { // Since the 'value' is -500, it will 'add' -500, making the bal $500 lower.
+            profile.updateBal(message.author.id, -500).then((i) => { // Since the 'value' is -500, it will 'add' -500, making the bal $500 lower.
                 message.channel.send(`**You paid your fine of $500!**\n**New Balance:** ${i.money}`);
             })
  
